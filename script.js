0edb50b468f68332fa1fc1eaa10fb2f8760bd2f5
Kamilla Aragao <kamilla.r.aragao@gmail.com>
2021-11-04 15:00:34 -0300

    starting game

---
+++ script.js
@@ -0,0 +1,81 @@
+const dino = document.querySelector('.dino'); 
+    /*procura onde tá a class dino*/
+const background = document.querySelector('.background');
+
+let isJumping = false;
+let position = 0;
+
+
+
+function handleKeyUp(event) {
+    if(event.keyCode === 32) {
+        if(!isJumping) {
+            jump();  
+      }   
+    }
+}
+
+function jump () {
+
+    isJumping =true;
+
+    let upInterval = setInterval(() => {
+
+       if(position >= 150) {
+        clearInterval(upInterval)
+
+        //descendo
+        let downInterval = setInterval(() => {
+
+            if(position <= 0) {
+                clearInterval(downInterval);
+                isJumping = false;
+            } else {
+                position -=20;
+                dino.style.bottom = position + 'px';
+            }
+        }, 20); 
+            }else {
+                //subindo
+                position += 20;
+                dino.style.bottom = position + 'px';
+        }
+    }, 20); /*executado a cada 20 milesegundo -loop*/ 
+}
+
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    
+    background.appendChild(cactus);
+ 
+   let leftInterval = setInterval (() => {
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if(cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            //gAME oVER
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class= "game-over">Fim de Jogo</h1>'
+
+        } else {
+
+            cactusPosition -= 07;
+            cactus.style.left = cactusPosition +'px';
+        } 
+   }, 20);
+
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
+    /*keyCode === 32 SIGNIFICA A TECLA ESPAÇO*/
+document.addEventListener('keyup', handleKeyUp); 
+    /*é uma forma mas expresiva de executar function() =>*/ 
+    /*keyup, quando tu pressiona a tecla e ela sobe a tecla sobe*/
+ 
